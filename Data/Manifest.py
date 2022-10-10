d4a6741558b8f3a08ea48c0e8a24184c683ad4c2
Virace <Virace@aliyun.com>
2022-10-10 21:20:05 +0800

    增加对英雄相关图片数据的获取

--- Data/Manifest.py
+++ Data/Manifest.py
@@ -4,7 +4,7 @@
 # @Site    : x-item.com
 # @Software: Pycharm
 # @Create  : 2022/8/15 23:56
-# @Update  : 2022/8/26 20:24
+# @Update  : 2022/10/10 21:20
 # @Detail  : 描述
 
 import json
@@ -28,6 +28,9 @@ class GameData:
         self.game_path = Path(GAME_PATH)
         self.out_path = Path(MANIFEST_PATH)
         self.region = GAME_REGION
+        self.data_path = self.game_path / 'LeagueClient' / 'Plugins' / 'rcp-be-lol-game-data'
+        self.wad_file_region = self.data_path / f'{format_region(self.region)}-assets.wad'
+        self.wad_file_default = self.data_path / 'default-assets.wad'
 
     def _get_out_path(self, before: [str, list[str]] = ''):
         if isinstance(before, str):
@@ -102,9 +105,6 @@ class GameData:
             new = path.replace(old, '')
             return os.path.join(self._get_out_path(), os.path.normpath(new))
 
-        data_path = self.game_path / 'LeagueClient' / 'Plugins' / 'rcp-be-lol-game-data'
-
-        wad_file = data_path / f'{format_region(self.region)}-assets.wad'
         hash_table = [
             f'plugins/rcp-be-lol-game-data/global/{self.region}/v1/champion-summary.json',
             f'plugins/rcp-be-lol-game-data/global/{self.region}/v1/skinlines.json',
@@ -112,11 +112,55 @@ class GameData:
             f'plugins/rcp-be-lol-game-data/global/{self.region}/v1/maps.json',
             f'plugins/rcp-be-lol-game-data/global/{self.region}/v1/universes.json'
         ]
-        WAD(wad_file).extract(hash_table, out_dir=output_file_name)
-        WAD(wad_file).extract(
+        WAD(self.wad_file_region).extract(hash_table, out_dir=output_file_name)
+        WAD(self.wad_file_region).extract(
             [f'plugins/rcp-be-lol-game-data/global/{self.region}/v1/champions/{item["id"]}.json' for item in
              self.get_summary()],
             out_dir=output_file_name)
+
+    def get_images(self):
+        """
+        获取英雄有关图片文件(头像、原画等)
+        :return:
+        """
+        _hash_list = []
+        _head = f'plugins/rcp-be-lol-game-data/global/default'
+
+        def fix_hash_path(path):
+            return f"{_head}/{path.replace('/lol-game-data/assets/', '')}"
+
+        def output_file_name(path):
+            old = 'plugins/rcp-be-lol-game-data/global/default/v1/'
+            loading = f'plugins/rcp-be-lol-game-data/global/default/ASSETS/Characters'
+            new = path.replace(old, '')
+            new = new.replace(loading, 'champion-loadscreen')
+
+            return os.path.join(self.out_path, 'images', os.path.normpath(new))
+
+        champions = self.get_summary()
+        for champion in champions:
+            cid = champion['id']
+
+            c_data = self.get_champion_detail_by_id(cid)
+            _hash_list.append(fix_hash_path(c_data['squarePortraitPath']))
+
+            for item in c_data['skins']:
+                # "splashPath": "/lol-game-data/assets/v1/champion-splashes/2/2000.jpg",
+                # "uncenteredSplashPath": "/lol-game-data/assets/v1/champion-splashes/uncentered/2/2000.jpg",
+                # "tilePath": "/lol-game-data/assets/v1/champion-tiles/2/2000.jpg",
+                # "loadScreenPath": "/lol-game-data/assets/ASSETS/Characters/Olaf/Skins/Base/OlafLoadScreen.jpg",
+
+                _hash_list.append(f'{_head}/v1/champion-splashes/{cid}/{item["id"]}.jpg')
+                _hash_list.append(f'{_head}/v1/champion-splashes/uncentered/{cid}/{item["id"]}.jpg')
+                _hash_list.append(f'{_head}/v1/champion-tiles/{cid}/{item["id"]}.jpg')
+                _hash_list.append(fix_hash_path(item['loadScreenPath']))
+
+                # 炫彩
+                if 'chromas' in item:
+                    _hash_list.append(f'{_head}/v1/chromaPath/{cid}/{item["id"]}.jpg')
+                    for chroma in item['chromas']:
+                        _hash_list.append(f'{_head}/v1/champion-chroma-images/{cid}/{item["id"]}/{chroma["id"]}.jpg')
+        WAD(self.wad_file_default).extract(_hash_list, out_dir=output_file_name)
 
     def get_game_version(self, default='99.99'):
         meta = self.game_path / 'Game' / 'code-metadata.json'
@@ -155,6 +199,6 @@ class GameData:
         ]
         WAD(wad_file).extract(hash_table, out_dir=output_file_name)
         WAD(wad_file).extract(
-            [f'plugins/rcp-be-lol-game-data/global/{_region}/v1/champions/{item["id"]}.json' for item in self.get_summary()],
+            [f'plugins/rcp-be-lol-game-data/global/{_region}/v1/champions/{item["id"]}.json' for item in
+             self.get_summary()],
             out_dir=output_file_name)
-
