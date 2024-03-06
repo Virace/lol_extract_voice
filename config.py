802fd17d65ea2ef12ade01ee59e188e7e5faa620
Virace <Virace@aliyun.com>
2024-03-06 19:35:04 +0800

    增加对系统环境变量的支持

--- config.py
+++ config.py
@@ -4,25 +4,25 @@
 # @Site    : x-item.com
 # @Software: Pycharm
 # @Create  : 2022/8/26 14:00
-# @Update  : 2022/8/27 13:12
+# @Update  : 2024/3/6 19:34
 # @Detail  : 描述
 
 import os
 
 # 游戏目录
-GAME_PATH = r''
+GAME_PATH: str = ''
 
 # 输出目录
-OUT_PATH = r''
+OUTPUT_PATH: str = ''
 
-# 要处理的语言
-GAME_REGION = 'zh_CN'
+# 要处理的区域文件
+GAME_REGION: str = 'zh_CN'
 
 # 排除处理的类型 (VO/SFX/MUSIC) 台词音频/效果音频/背景音乐
-EXCLUDE_TYPE = ['SFX']
+EXCLUDE_TYPE: list = ['SFX']
 
 # vgmstream cli程序, 用来转码, 如果不提供则默认输出为wem格式。
-VGMSTREAM_PATH = ''
+VGMSTREAM_PATH: str = ''
 
 ######################################################
 #                ！！！！！！！！！！                #
@@ -30,23 +30,42 @@ VGMSTREAM_PATH = ''
 #                ！！！！！！！！！！                #
 ######################################################
 
+# 从环境变量中获取 ENV_ONLY，默认为 False
+ENV_ONLY = os.getenv('LOL_ENV_ONLY', '0').lower() not in ['0', 'false']
+
+# 设置需要获取的参数列表
+params = ['GAME_PATH', 'GAME_REGION', 'OUTPUT_PATH', 'EXCLUDE_TYPE', 'VGMSTREAM_PATH']
+
+# 根据 ENV_ONLY 的值选择获取参数的方式
+if ENV_ONLY:
+    # 如果 ENV_ONLY 为 True，则所有参数都从环境变量中获取
+    for param in params:
+        if param == 'EXCLUDE_TYPE':
+            globals()[param] = os.getenv(f'LOL_{param}', '').split(',')
+        else:
+            globals()[param] = os.getenv(f'LOL_{param}', '')
+else:
+    # 否则，优先使用上面常量，如果为 None，则从环境变量中获取
+    for param in params:
+        globals()[param] = globals()[param] or os.getenv(f'LOL_{param}')
+
 # 音频目录, 最终解包生成的音频文件都放在这
-AUDIO_PATH = os.path.join(OUT_PATH, 'audios')
+AUDIO_PATH = os.path.join(OUTPUT_PATH, 'audios')
 
 # 缓存目录, 解包生成的一些文件会放在这里, 可以删除
-TEMP_PATH = os.path.join(OUT_PATH, 'temps')
+TEMP_PATH = os.path.join(OUTPUT_PATH, 'temps')
 
 # 日志目录, 一些文件解析错误不会关闭程序而是记录在日志中
-LOG_PATH = os.path.join(OUT_PATH, 'logs')
+LOG_PATH = os.path.join(OUTPUT_PATH, 'logs')
 
 # 哈希目录, 存放所有与 k,v 相关数据
-HASH_PATH = os.path.join(OUT_PATH, 'hashes')
+HASH_PATH = os.path.join(OUTPUT_PATH, 'hashes')
 
 # 有关于游戏内的数据文件
-MANIFEST_PATH = os.path.join(OUT_PATH, 'manifest')
+MANIFEST_PATH = os.path.join(OUTPUT_PATH, 'manifest')
 
 # 游戏版本文件, 用来记录当前解包文件的版本
-LOCAL_VERSION_FILE = os.path.join(OUT_PATH, 'game_version')
+LOCAL_VERSION_FILE = os.path.join(OUTPUT_PATH, 'game_version')
 
 # 游戏英雄文件目录 (Game/DATA/FINAL/Champions)
 GAME_CHAMPION_PATH = os.path.join(GAME_PATH, 'Game', 'DATA', 'FINAL', 'Champions')
