477d3e3ce6a5778a5333708c2c3010ad05fababd
Virace <Virace@aliyun.com>
2024-03-12 09:21:13 +0800

    增加从文件(默认为`.lol.env`)中读取环境变量。

--- config.py
+++ config.py
@@ -4,16 +4,21 @@
 # @Site    : x-item.com
 # @Software: Pycharm
 # @Create  : 2022/8/26 14:00
-# @Update  : 2024/3/6 19:34
+# @Update  : 2024/3/12 9:19
 # @Detail  : 描述
 
 import os
+from typing import Union
+
+from dotenv import load_dotenv
+
+StrPath = Union[str, 'os.PathLike[str]']
 
 # 游戏目录
-GAME_PATH: str = ''
+GAME_PATH: StrPath = ''
 
 # 输出目录
-OUTPUT_PATH: str = ''
+OUTPUT_PATH: StrPath = ''
 
 # 要处理的区域文件
 GAME_REGION: str = 'zh_CN'
@@ -31,13 +36,23 @@ VGMSTREAM_PATH: str = ''
 ######################################################
 
 # 从环境变量中获取 ENV_ONLY，默认为 False
-ENV_ONLY = os.getenv('LOL_ENV_ONLY', '0').lower() not in ['0', 'false']
+ENV_ONLY: bool = os.getenv('LOL_ENV_ONLY', '0').lower() not in ['0', 'false']
+
+# 从环境变量中获取 ENV_PATH，默认为 .lol.env
+ENV_PATH: StrPath = os.getenv('LOL_ENV_PATH', '.lol.env')
+
+# 是否使用ENV_PATH中的配置覆盖环境变量中的配置，默认为 False
+ENV_OVERRIDE: bool = os.getenv('LOL_ENV_OVERRIDE', '0').lower() not in ['0', 'false']
 
 # 设置需要获取的参数列表
 params = ['GAME_PATH', 'GAME_REGION', 'OUTPUT_PATH', 'EXCLUDE_TYPE', 'VGMSTREAM_PATH']
 
 # 根据 ENV_ONLY 的值选择获取参数的方式
 if ENV_ONLY:
+
+    # 从.lol.env文件中获取参数
+    load_dotenv(dotenv_path=ENV_PATH, override=ENV_OVERRIDE)
+
     # 如果 ENV_ONLY 为 True，则所有参数都从环境变量中获取
     for param in params:
         if param == 'EXCLUDE_TYPE':
@@ -48,6 +63,13 @@ else:
     # 否则，优先使用上面常量，如果为 None，则从环境变量中获取
     for param in params:
         globals()[param] = globals()[param] or os.getenv(f'LOL_{param}')
+
+# 如果GAME_PATH 或 OUTPUT_PATH 为空则抛出异常
+if not GAME_PATH:
+    raise ValueError('GAME_PATH不能为空')
+
+if not OUTPUT_PATH:
+    raise ValueError('OUTPUT_PATH不能为空')
 
 # 音频目录, 最终解包生成的音频文件都放在这
 AUDIO_PATH = os.path.join(OUTPUT_PATH, 'audios')
