f2384bdd3d8c4f985322916f8e79fe0b3db67ec1
Virace <Virace@aliyun.com>
2024-03-12 13:28:20 +0800

    typing

---
+++ Utils/type_hints.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author  : Virace
+# @Email   : Virace@aliyun.com
+# @Site    : x-item.com
+# @Software: Pycharm
+# @Create  : 2024/3/12 12:39
+# @Update  : 2024/3/12 12:46
+# @Detail  : 
+
+from typing import Union
+
+StrPath = Union[str, 'os.PathLike[str]']
