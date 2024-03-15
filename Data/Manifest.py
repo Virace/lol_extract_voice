bd0cc59e14799c760b1a6d52d2a2b0ae68110e4a
Virace <Virace@aliyun.com>
2024-03-15 09:28:17 +0800

    去os.path，black格式化，未测试

--- Data/Manifest.py
+++ Data/Manifest.py
@@ -4,11 +4,10 @@
 # @Site    : x-item.com
 # @Software: Pycharm
 # @Create  : 2022/8/15 23:56
-# @Update  : 2024/3/13 19:44
+# @Update  : 2024/3/15 9:27
 # @Detail  : 游戏数据
 
 import json
-import os
 import re
 import traceback
 from pathlib import Path
@@ -26,89 +25,118 @@ class GameData:
     获取游戏相关数据
     """
 
-    def __init__(self, game_path: StrPath,
-                 manifest_path: StrPath, region: str = 'zh_CN'):
+    def __init__(
+        self, game_path: StrPath, manifest_path: StrPath, region: str = "zh_CN"
+    ):
         """
         :param region: 地区
         """
         self.game_path = Path(game_path)
         self.out_path = Path(manifest_path)
         self.region = region
-        if self.region.lower() == 'en_us':
-            self.region = 'default'
-        self.data_path = self.game_path / 'LeagueClient' / 'Plugins' / 'rcp-be-lol-game-data'
-        self.wad_file_region = self.data_path / f'{format_region(self.region)}-assets.wad'
-        self.wad_file_default = self.data_path / 'default-assets.wad'
+        if self.region.lower() == "en_us":
+            self.region = "default"
+        self.data_path = (
+            self.game_path / "LeagueClient" / "Plugins" / "rcp-be-lol-game-data"
+        )
+        self.wad_file_region = (
+            self.data_path / f"{format_region(self.region)}-assets.wad"
+        )
+        self.wad_file_default = self.data_path / "default-assets.wad"
 
-        self._version_api = 'https://ddragon.leagueoflegends.com/api/versions.json'
+        self._version_api = "https://ddragon.leagueoflegends.com/api/versions.json"
 
         # 游戏英雄文件目录 (Game/DATA/FINAL/Champions)
-        self.GAME_CHAMPION_PATH = os.path.join(self.game_path,
-                                               'Game', 'DATA', 'FINAL', 'Champions')
+        self.GAME_CHAMPION_PATH = (
+            self.game_path / "Game" / "DATA" / "FINAL" / "Champions"
+        )
 
         # 游戏地图(公共)文件目录 (Game/DATA/FINAL/Maps/Shipping)
-        self.GAME_MAPS_PATH = os.path.join(self.game_path, 'Game', 'DATA', 'FINAL', 'Maps', 'Shipping')
+        self.GAME_MAPS_PATH = (
+            self.game_path / "Game" / "DATA" / "FINAL" / "Maps" / "Shipping"
+        )
 
-    def _get_out_path(self, files: [str, list[str]] = ''):
+    def _get_out_path(self, files: [str, list[str]] = ""):
         """
         获取输出路径
         :param files: 文件, 可传入数组 则为多级目录
-        :return:
+        :return: 完整的文件路径
         """
         if isinstance(files, str):
             files = [files]
+        elif not isinstance(files, list):
+            raise TypeError("files 必须是字符串或字符串列表")
         return (self.out_path / self.region).joinpath(*files)
 
-    def _open_file(self, filename):
+    def _open_file(self, filename: [str, list[str]]) -> dict:
+        """
+        打开并读取 JSON 文件
+        :param filename: 文件名
+        :return: 文件内容
+        """
         file = self._get_out_path(filename)
-        if not os.path.exists(file):
-            logger.warning(f'{file}不存在')
-            return {}
         try:
-            with open(file, encoding='utf-8') as f:
+            with open(file, encoding="utf-8") as f:
                 return json.load(f)
-        except Exception:
-            e = traceback.format_exc()
-            logger.warning(e)
-            return {}
+        except FileNotFoundError:
+            logger.warning(f"文件 {file} 不存在")
+        except json.JSONDecodeError:
+            logger.warning(f"无法解析文件 {file}，文件可能不是有效的 JSON 格式")
+        except IOError:
+            logger.warning(f"无法读取文件 {file}")
+        except Exception as e:
+            logger.warning(f"打开文件 {file} 时发生未知错误: {str(e)}")
+            logger.debug(traceback.format_exc())
+        return {}
 
-    def get_summary(self, ):
+    def get_summary(
+        self,
+    ):
         """
         获取英雄列表
         :return:
         """
-        return self._open_file('champion-summary.json')
+        return self._open_file("champion-summary.json")
 
-    def get_skins(self, ):
+    def get_skins(
+        self,
+    ):
         """
         获取皮肤列表
         :return:
         """
-        return self._open_file('skins.json')
+        return self._open_file("skins.json")
 
-    def get_skinlines(self, ):
+    def get_skinlines(
+        self,
+    ):
         """
         获取皮肤系列列表
         :return:
         """
-        temp = self._open_file('skinlines.json')
-        result = {item['id']: item['name'] for item in temp}
+        temp = self._open_file("skinlines.json")
+        result = {item["id"]: item["name"] for item in temp}
         return result
 
-    def get_maps(self, ):
+    def get_maps(
+        self,
+    ):
         """
         获取地图列表
         :return:
         """
-        return self._open_file('maps.json')
+        return self._open_file("maps.json")
 
-    def get_champion_detail_by_id(self, cid, ):
+    def get_champion_detail_by_id(
+        self,
+        cid,
+    ):
         """
         根据英雄ID获取英雄详情
         :param cid:
         :return:
         """
-        return self._open_file(['champions', f'{cid}.json'])
+        return self._open_file(["champions", f"{cid}.json"])
 
     def get_champion_name(self, name, chinese=True):
         """
@@ -119,13 +147,15 @@ class GameData:
         """
         summary = self.get_summary()
         for item in summary:
-            if item['alias'].lower() == name.lower():
+            if item["alias"].lower() == name.lower():
                 if chinese:
-                    return item['alias'], item['name']
+                    return item["alias"], item["name"]
                 else:
-                    return item['alias']
+                    return item["alias"]
 
-    def get_champions_name(self, ):
+    def get_champions_name(
+        self,
+    ):
         """
         获取英雄名字, 说是名字, 其实json中是title
         :return:
@@ -133,62 +163,71 @@ class GameData:
         res = {}
         summary = self.get_summary()
         for item in summary:
-            if item['id'] == -1:
+            if item["id"] == -1:
                 continue
 
-            this = self.get_champion_detail_by_id(item['id'])
-            res[item['alias']] = this['title']
+            this = self.get_champion_detail_by_id(item["id"])
+            res[item["alias"]] = this["title"]
         return res
 
-    def get_champions_alias(self, ):
+    def get_champions_alias(
+        self,
+    ):
         """
         获取英雄代号, 说是代号，其实json中是name
         :return:
         """
-        return {item['alias'].lower(): item['name'] for item in self.get_summary()}
+        return {item["alias"].lower(): item["name"] for item in self.get_summary()}
 
-    def get_champions_id(self, ):
+    def get_champions_id(
+        self,
+    ):
         """
         获取英雄ID
         :return:
         """
-        return [item['id'] for item in self.get_summary()]
+        return [item["id"] for item in self.get_summary()]
 
-    def get_maps_id(self, ):
+    def get_maps_id(
+        self,
+    ):
         """
         获取地图ID
         :return:
         """
-        return [item['id'] for item in self.get_maps()]
+        return [item["id"] for item in self.get_maps()]
 
     def get_manifest(self):
         """
         获取文件清单
         :return:
         """
-        logger.trace('获取文件清单')
-        if self.region == 'en_us':
-            region = 'default'
+        logger.trace("获取文件清单")
+        if self.region == "en_us":
+            region = "default"
 
-        def output_file_name(path):
-            old = f'plugins/rcp-be-lol-game-data/global/{region}/v1/'
-            new = path.replace(old, '')
-            return os.path.join(self._get_out_path(), os.path.normpath(new))
+        def output_file_name(path: str):
+            old = f"plugins/rcp-be-lol-game-data/global/{region}/v1/"
+            new = path.replace(old, "")
+            return self._get_out_path() / Path(new)
 
         hash_table = [
-            f'plugins/rcp-be-lol-game-data/global/{self.region}/v1/champion-summary.json',
-            f'plugins/rcp-be-lol-game-data/global/{self.region}/v1/skinlines.json',
-            f'plugins/rcp-be-lol-game-data/global/{self.region}/v1/skins.json',
-            f'plugins/rcp-be-lol-game-data/global/{self.region}/v1/maps.json',
-            f'plugins/rcp-be-lol-game-data/global/{self.region}/v1/items.json',
-            f'plugins/rcp-be-lol-game-data/global/{self.region}/v1/universes.json'
+            f"plugins/rcp-be-lol-game-data/global/{self.region}/v1/champion-summary.json",
+            f"plugins/rcp-be-lol-game-data/global/{self.region}/v1/skinlines.json",
+            f"plugins/rcp-be-lol-game-data/global/{self.region}/v1/skins.json",
+            f"plugins/rcp-be-lol-game-data/global/{self.region}/v1/maps.json",
+            f"plugins/rcp-be-lol-game-data/global/{self.region}/v1/items.json",
+            f"plugins/rcp-be-lol-game-data/global/{self.region}/v1/universes.json",
         ]
         WAD(self.wad_file_region).extract(hash_table, out_dir=output_file_name)
         WAD(self.wad_file_region).extract(
-            [f'plugins/rcp-be-lol-game-data/global/{self.region}/v1/champions/'
-             f'{item["id"]}.json' for item in
-             self.get_summary()],
-            out_dir=output_file_name)
+            [
+                f"plugins/rcp-be-lol-game-data/global/{self.region}/v1/champions/"
+                f'{item["id"]}.json'
+                for item in self.get_summary()
+            ],
+            out_dir=output_file_name,
+        )
 
     def get_images(self):
         """
@@ -196,55 +235,61 @@ class GameData:
         :return:
         """
         _hash_list = []
-        _head = 'plugins/rcp-be-lol-game-data/global/default'
+        _head = "plugins/rcp-be-lol-game-data/global/default"
 
         def fix_hash_path(path):
             return f"{_head}/{path.replace('/lol-game-data/assets/', '')}"
 
         def output_file_name(path):
-            old = 'plugins/rcp-be-lol-game-data/global/default/v1/'
-            loading = 'plugins/rcp-be-lol-game-data/global/default/ASSETS/Characters'
-            new = path.replace(old, '')
-            new = new.replace(loading, 'champion-loadscreen')
+            old = "plugins/rcp-be-lol-game-data/global/default/v1/"
+            loading = "plugins/rcp-be-lol-game-data/global/default/ASSETS/Characters"
+            new = path.replace(old, "")
+            new = new.replace(loading, "champion-loadscreen")
 
-            return os.path.join(self.out_path, 'images', os.path.normpath(new))
+            return self.out_path / "images" / Path(new)
 
         champions = self.get_summary()
         for champion in champions:
-            cid = champion['id']
+            cid = champion["id"]
 
             c_data = self.get_champion_detail_by_id(cid)
-            _hash_list.append(fix_hash_path(c_data['squarePortraitPath']))
+            _hash_list.append(fix_hash_path(c_data["squarePortraitPath"]))
 
-            for item in c_data['skins']:
+            for item in c_data["skins"]:
                 # "splashPath": "/lol-game-data/assets/v1/champion-splashes/2/2000.jpg",
                 # "uncenteredSplashPath": "/lol-game-data/assets/v1/champion-splashes/uncentered/2/2000.jpg",
                 # "tilePath": "/lol-game-data/assets/v1/champion-tiles/2/2000.jpg",
                 # "loadScreenPath": "/lol-game-data/assets/ASSETS/Characters/Olaf/Skins/Base/OlafLoadScreen.jpg",
 
-                _hash_list.append(f'{_head}/v1/champion-splashes/{cid}/{item["id"]}.jpg')
-                _hash_list.append(f'{_head}/v1/champion-splashes/uncentered/{cid}/{item["id"]}.jpg')
+                _hash_list.append(
+                    f'{_head}/v1/champion-splashes/{cid}/{item["id"]}.jpg'
+                )
+                _hash_list.append(
+                    f'{_head}/v1/champion-splashes/uncentered/{cid}/{item["id"]}.jpg'
+                )
                 _hash_list.append(f'{_head}/v1/champion-tiles/{cid}/{item["id"]}.jpg')
-                _hash_list.append(fix_hash_path(item['loadScreenPath']))
+                _hash_list.append(fix_hash_path(item["loadScreenPath"]))
 
                 # 炫彩
-                if 'chromas' in item:
+                if "chromas" in item:
                     _hash_list.append(f'{_head}/v1/chromaPath/{cid}/{item["id"]}.jpg')
-                    for chroma in item['chromas']:
-                        _hash_list.append(f'{_head}/v1/champion-chroma-images/{cid}/{item["id"]}/{chroma["id"]}.jpg')
+                    for chroma in item["chromas"]:
+                        _hash_list.append(
+                            f'{_head}/v1/champion-chroma-images/{cid}/{item["id"]}/{chroma["id"]}.jpg'
+                        )
         WAD(self.wad_file_default).extract(_hash_list, out_dir=output_file_name)
 
-    def get_game_version(self, default='99.99'):
+    def get_game_version(self, default="99.99"):
         """
         获取游戏版本
         :param default:
         :return:
         """
-        meta = self.game_path / 'Game' / 'content-metadata.json'
-        if os.path.exists(meta):
-            with open(meta, encoding='utf-8') as f:
+        meta = self.game_path / "Game" / "content-metadata.json"
+        if meta.exists():
+            with open(meta, encoding="utf-8") as f:
                 data = json.load(f)
-            version_v = data['version']
+            version_v = data["version"]
         else:
             return default
         if m := re.match(r"^(\d+\.\d+)\.", version_v):
@@ -264,30 +309,33 @@ class GameData:
         """
         _region = self.region
         # 游戏内英文文件作为default默认存在
-        if self.region == 'en_us':
-            _region = 'default'
+        if self.region == "en_us":
+            _region = "default"
 
         def output_file_name(path):
-            old = f'plugins/rcp-be-lol-game-data/global/{_region}/v1/'
-            new = path.replace(old, '')
-            return os.path.join(self.out_path, _region, os.path.normpath(new))
+            old = f"plugins/rcp-be-lol-game-data/global/{_region}/v1/"
+            new = path.replace(old, "")
+            return self.out_path / _region / Path(new)
 
-        data_path = os.path.join(self.game_path, 'LeagueClient', 'Plugins', 'rcp-be-lol-game-data')
+        data_path = self.game_path / "LeagueClient" / "Plugins" / "rcp-be-lol-game-data"
 
-        wad_file = os.path.join(data_path, f'{format_region(_region)}-assets.wad')
+        wad_file = data_path / f"{format_region(_region)}-assets.wad"
         hash_table = [
-            f'plugins/rcp-be-lol-game-data/global/{_region}/v1/champion-summary.json',
-            f'plugins/rcp-be-lol-game-data/global/{_region}/v1/skinlines.json',
-            f'plugins/rcp-be-lol-game-data/global/{_region}/v1/skins.json',
-            f'plugins/rcp-be-lol-game-data/global/{_region}/v1/maps.json',
-            f'plugins/rcp-be-lol-game-data/global/{_region}/v1/items.json',
-            f'plugins/rcp-be-lol-game-data/global/{_region}/v1/universes.json',
+            f"plugins/rcp-be-lol-game-data/global/{_region}/v1/champion-summary.json",
+            f"plugins/rcp-be-lol-game-data/global/{_region}/v1/skinlines.json",
+            f"plugins/rcp-be-lol-game-data/global/{_region}/v1/skins.json",
+            f"plugins/rcp-be-lol-game-data/global/{_region}/v1/maps.json",
+            f"plugins/rcp-be-lol-game-data/global/{_region}/v1/items.json",
+            f"plugins/rcp-be-lol-game-data/global/{_region}/v1/universes.json",
         ]
         WAD(wad_file).extract(hash_table, out_dir=output_file_name)
         WAD(wad_file).extract(
-            [f'plugins/rcp-be-lol-game-data/global/{_region}/v1/champions/{item["id"]}.json' for item in
-             self.get_summary()],
-            out_dir=output_file_name)
+            [
+                f'plugins/rcp-be-lol-game-data/global/{_region}/v1/champions/{item["id"]}.json'
+                for item in self.get_summary()
+            ],
+            out_dir=output_file_name,
+        )
 
 
 def compare_version(version1: str, version2: str) -> None:
@@ -299,18 +347,21 @@ def compare_version(version1: str, version2: str) -> None:
     """
     # 检查输入格式
     if not is_valid_version(version1) or not is_valid_version(version2):
-        logger.error("版本号格式不正确，请使用 '大版本.小版本' 或 '大版本.小版本.修订号' 格式。")
+        logger.error(
+            "版本号格式不正确，请使用 '大版本.小版本' 或 '大版本.小版本.修订号' 格式。"
+        )
         return
 
-    version1_parts = version1.split('.')
-    version2_parts = version2.split('.')
+    version1_parts = version1.split(".")
+    version2_parts = version2.split(".")
 
     major_version1, minor_version1 = int(version1_parts[0]), int(version1_parts[1])
     major_version2, minor_version2 = int(version2_parts[0]), int(version2_parts[1])
 
     if major_version1 != major_version2:
         raise ValueError(
-            f"大版本不同，无法比较。版本号分别为: {version1} 和 {version2}")
+            f"大版本不同，无法比较。版本号分别为: {version1} 和 {version2}"
+        )
     elif minor_version1 != minor_version2:
         logger.warning(f"小版本不同，请注意。版本号分别为: {version1} 和 {version2}")
 
@@ -318,7 +369,7 @@ def compare_version(version1: str, version2: str) -> None:
 
 
 def is_valid_version(version: str) -> bool:
-    parts = version.split('.')
+    parts = version.split(".")
     if len(parts) < 2 or len(parts) > 3:
         return False
     try:
