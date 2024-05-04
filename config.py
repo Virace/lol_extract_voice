2107d02d7a1f6821e5c0110220b4547bd322b21a
Virace <Virace@aliyun.com>
2024-05-05 06:54:02 +0800

    更改配置文件逻辑

--- config.py
+++ config.py
@@ -4,99 +4,129 @@
 # @Site    : x-item.com
 # @Software: Pycharm
 # @Create  : 2022/8/26 14:00
-# @Update  : 2024/3/12 14:21
+# @Update  : 2024/5/5 6:41
 # @Detail  : config.py
 
+import json
 import os
 
 from dotenv import load_dotenv
+from typing import Dict
+from pathlib import Path
 
 from Utils.type_hints import StrPath
 
-# 游戏目录
-GAME_PATH: StrPath = ''
 
-# 输出目录
-OUTPUT_PATH: StrPath = ''
+class Config:
+    _instance = None
 
-# 要处理的区域文件
-GAME_REGION: str = 'zh_CN'
+    # 增加前缀防止冲突
+    env_prefix: str = "LOL_"
 
-# 排除处理的类型 (VO/SFX/MUSIC) 台词音频/效果音频/背景音乐
-EXCLUDE_TYPE: list = ['SFX']
+    # 配置路径，默认为当前目录下的 .lol.env 文件
+    env_path: StrPath = os.getenv("LOL_ENV_PATH", ".lol.env")
 
-# vgmstream cli程序, 用来转码, 如果不提供则默认输出为wem格式。
-VGMSTREAM_PATH: str = ''
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super(Config, cls).__new__(cls)
+            cls._load_and_set_env(cls._instance)
 
-######################################################
-#                ！！！！！！！！！！                #
-#                以下配置无需手动修改                #
-#                ！！！！！！！！！！                #
-######################################################
+        return cls._instance
 
-# 从环境变量中获取 ENV_ONLY，默认为 False
-ENV_ONLY: bool = os.getenv('LOL_ENV_ONLY', '0').lower() not in ['0', 'false']
+    @classmethod
+    def _get_env(cls, key, metadata: Dict):
+        """
+        获取环境变量
+        :param key:
+        :param metadata:
+        :return:
+        """
 
-# 从环境变量中获取 ENV_PATH，默认为 .lol.env
-ENV_PATH: StrPath = os.getenv('LOL_ENV_PATH', '.lol.env')
+        type_func = {
+            "str": lambda x: x,
+            "path": lambda x: Path(x) if x else '',
+            "list": lambda x: x.split(",") if x else [],
+        }
 
-# 是否使用ENV_PATH中的配置覆盖环境变量中的配置，默认为 False
-ENV_OVERRIDE: bool = os.getenv('LOL_ENV_OVERRIDE', '0').lower() not in ['0', 'false']
+        required = metadata.get("required", False)
+        default = metadata.get("default", None)
+        _type = metadata.get("type", "str")
 
-# 设置需要获取的参数列表
-params = ['GAME_PATH', 'GAME_REGION', 'OUTPUT_PATH', 'EXCLUDE_TYPE', 'VGMSTREAM_PATH']
+        data = type_func[_type](os.getenv(f"{cls.env_prefix}{key}", default))
 
-# 根据 ENV_ONLY 的值选择获取参数的方式
-if ENV_ONLY:
+        if required and not data:
+            raise ValueError(f"{cls.env_prefix}{key}不能为空")
 
-    # 从.lol.env文件中获取参数
-    load_dotenv(dotenv_path=ENV_PATH, override=ENV_OVERRIDE)
+        return data
 
-    # 如果 ENV_ONLY 为 True，则所有参数都从环境变量中获取
-    for param in params:
-        if param == 'EXCLUDE_TYPE':
-            globals()[param] = os.getenv(f'LOL_{param}', '').split(',')
-        else:
-            globals()[param] = os.getenv(f'LOL_{param}', '')
-else:
-    # 否则，优先使用上面常量，如果为 None，则从环境变量中获取
-    for param in params:
-        globals()[param] = globals()[param] or os.getenv(f'LOL_{param}')
+    @classmethod
+    def _load_and_set_env(cls, instance):
+        """
+        加载环境变量并设置实例属性
+        :param instance: 实例
+        :return:
+        """
 
-# 如果GAME_PATH 或 OUTPUT_PATH 为空则抛出异常
-if not GAME_PATH:
-    raise ValueError('GAME_PATH不能为空')
+        load_dotenv(dotenv_path=cls.env_path, override=True)
+        cls.params = json.loads((Path("params.json")).read_text())
+        for param, metadata in cls.params.items():
+            data = cls._get_env(param, metadata)
 
-if not OUTPUT_PATH:
-    raise ValueError('OUTPUT_PATH不能为空')
+            # 属性赋值时去掉前缀
+            setattr(instance, param.replace(cls.env_prefix, ""), data)
 
-# 音频目录, 最终解包生成的音频文件都放在这
-AUDIO_PATH = os.path.join(OUTPUT_PATH, 'audios')
+        out_path = getattr(instance, "OUTPUT_PATH")
+        game_path = getattr(instance, "GAME_PATH")
 
-# 缓存目录, 解包生成的一些文件会放在这里, 可以删除
-TEMP_PATH = os.path.join(OUTPUT_PATH, 'temps')
+        # 音频目录, 最终解包生成的音频文件都放在这
+        setattr(instance, "AUDIO_PATH", out_path / "audios")
 
-# 日志目录, 一些文件解析错误不会关闭程序而是记录在日志中
-LOG_PATH = os.path.join(OUTPUT_PATH, 'logs')
+        # 缓存目录, 解包生成的一些文件会放在这里, 可以删除
+        setattr(instance, "TEMP_PATH", out_path / "temps")
 
-# 哈希目录, 存放所有与 k,v 相关数据
-HASH_PATH = os.path.join(OUTPUT_PATH, 'hashes')
+        # 日志目录, 一些文件解析错误不会关闭程序而是记录在日志中
+        setattr(instance, "LOG_PATH", out_path / "logs")
 
-# 有关于游戏内的数据文件
-MANIFEST_PATH = os.path.join(OUTPUT_PATH, 'manifest')
+        # 哈希目录, 存放所有与 k,v 相关数据
+        setattr(instance, "HASH_PATH", out_path / "hashes")
 
-# 游戏版本文件, 用来记录当前解包文件的版本
-LOCAL_VERSION_FILE = os.path.join(OUTPUT_PATH, 'game_version')
+        # 有关于游戏内的数据文件
+        setattr(instance, "MANIFEST_PATH", out_path / "manifest")
 
-# 游戏英雄文件目录 (Game/DATA/FINAL/Champions)
-GAME_CHAMPION_PATH = os.path.join(GAME_PATH, 'Game', 'DATA', 'FINAL', 'Champions')
+        # 游戏版本文件, 用来记录当前解包文件的版本
+        setattr(instance, "LOCAL_VERSION_FILE", out_path / "game_version")
 
-# 游戏地图(公共)文件目录 (Game/DATA/FINAL/Maps/Shipping)
-GAME_MAPS_PATH = os.path.join(GAME_PATH, 'Game', 'DATA', 'FINAL', 'Maps', 'Shipping')
+        # 游戏英雄文件目录 (Game/DATA/FINAL/Champions)
+        setattr(
+            instance,
+            "GAME_CHAMPION_PATH",
+            game_path / "Game" / "DATA" / "FINAL" / "Champions",
+        )
 
-# 游戏大厅资源目录
-GAME_LCU_PATH = os.path.join(GAME_PATH, 'LeagueClient', 'Plugins', 'rcp-be-lol-game-data')
+        # 游戏地图(公共)文件目录 (Game/DATA/FINAL/Maps/Shipping)
+        setattr(
+            instance,
+            "GAME_MAPS_PATH",
+            game_path / "Game" / "DATA" / "FINAL" / "Maps" / "Shipping",
+        )
 
-# 修正
-if GAME_REGION.lower() == 'en_us':
-    GAME_REGION = 'default'
+        # 游戏大厅资源目录
+        setattr(
+            instance,
+            "GAME_LCU_PATH",
+            game_path / "LeagueClient" / "Plugins" / "rcp-be-lol-game-data",
+        )
+
+        # 修正
+        if getattr(instance, "GAME_REGION").lower() == "en_us":
+            setattr(instance, "GAME_REGION", "default")
+
+    def reload_config(self):
+        """
+        重新加载配置
+        :return:
+        """
+        self._load_and_set_env(self)
+
+
+config_instance = Config()
