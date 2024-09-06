d61677dcce78eca2e955ffd9cd2c2cdec364c293
Virace <Virace@aliyun.com>
2024-09-06 13:47:36 +0800

    资源类

---
+++ src/lol_audio_unpack/Data/resource.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author  : Virace
+# @Email   : Virace@aliyun.com
+# @Site    : x-item.com
+# @Software: Pycharm
+# @Create  : 2024/9/6 13:47
+# @Update  : 2024/9/6 13:47
+# @Detail  : 
