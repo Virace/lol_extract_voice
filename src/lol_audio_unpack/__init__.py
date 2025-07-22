2cc9a28029031c4f65fdea35631c00aaa0fe4142
Virace <Virace@aliyun.com>
2025-07-23 05:31:46 +0800

    更新文件头信息，修改创建和更新时间，添加注释以增强可读性。

--- src/lol_audio_unpack/__init__.py
+++ src/lol_audio_unpack/__init__.py
@@ -1,8 +1,10 @@
-# -*- coding: utf-8 -*-
+# 🐍 Although never is often better than *right* now.
+# 🐼 然而不假思索还不如不做
 # @Author  : Virace
 # @Email   : Virace@aliyun.com
 # @Site    : x-item.com
 # @Software: Pycharm
-# @Create  : 2024/9/3 10:21
-# @Update  : 2024/9/3 10:21
+# @Create  : 2024/9/3 10:14
+# @Update  : 2025/7/23 5:31
 # @Detail  : 
+
